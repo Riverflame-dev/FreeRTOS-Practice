7c021d1f7c766aed982d2478ae9f7462af16684b
Yilei Li <97814993+Riverflame-dev@users.noreply.github.com>
2022-09-26 16:06:14 -0400

    Add files via upload

---
+++ Graphics, LCD, ADC, Timer and Interpreter/Lab1Part3TestMain.c
@@ -0,0 +1,146 @@
+// ST7735TestMain.c
+// Runs on LM4F120/TM4C123
+// Test the functions in ST7735.c by printing basic
+// patterns to the LCD.
+//    16-bit color, 128 wide by 160 high LCD
+// Daniel Valvano
+// March 30, 2015
+
+/* This example accompanies the book
+   "Embedded Systems: Real Time Interfacing to ARM Cortex M Microcontrollers",
+   ISBN: 978-1463590154, Jonathan Valvano, copyright (c) 2014
+
+ Copyright 2015 by Jonathan W. Valvano, valvano@mail.utexas.edu
+    You may use, edit, run or distribute this file
+    as long as the above copyright notice remains
+ THIS SOFTWARE IS PROVIDED "AS IS".  NO WARRANTIES, WHETHER EXPRESS, IMPLIED
+ OR STATUTORY, INCLUDING, BUT NOT LIMITED TO, IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE APPLY TO THIS SOFTWARE.
+ VALVANO SHALL NOT, IN ANY CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL,
+ OR CONSEQUENTIAL DAMAGES, FOR ANY REASON WHATSOEVER.
+ For more information about my classes, my research, and my books, see
+ http://users.ece.utexas.edu/~valvano/
+ */
+
+// hardware connections
+// **********ST7735 TFT and SDC*******************
+// ST7735
+// Backlight (pin 10) connected to +3.3 V
+// MISO (pin 9) unconnected
+// SCK (pin 8) connected to PA2 (SSI0Clk)
+// MOSI (pin 7) connected to PA5 (SSI0Tx)
+// TFT_CS (pin 6) connected to PA3 (SSI0Fss)
+// CARD_CS (pin 5) unconnected
+// Data/Command (pin 4) connected to PA6 (GPIO), high for data, low for command
+// RESET (pin 3) connected to PA7 (GPIO)
+// VCC (pin 2) connected to +3.3 V
+// Gnd (pin 1) connected to ground
+
+// **********wide.hk ST7735R with ADXL345 accelerometer *******************
+// Silkscreen Label (SDC side up; LCD side down) - Connection
+// VCC  - +3.3 V
+// GND  - Ground
+// !SCL - PA2 Sclk SPI clock from microcontroller to TFT or SDC
+// !SDA - PA5 MOSI SPI data from microcontroller to TFT or SDC
+// DC   - PA6 TFT data/command
+// RES  - PA7 TFT reset
+// CS   - PA3 TFT_CS, active low to enable TFT
+// *CS  - (NC) SDC_CS, active low to enable SDC
+// MISO - (NC) MISO SPI data from SDC to microcontroller
+// SDA  – (NC) I2C data for ADXL345 accelerometer
+// SCL  – (NC) I2C clock for ADXL345 accelerometer
+// SDO  – (NC) I2C alternate address for ADXL345 accelerometer
+// Backlight + - Light, backlight connected to +3.3 V
+
+// **********wide.hk ST7735R with ADXL335 accelerometer *******************
+// Silkscreen Label (SDC side up; LCD side down) - Connection
+// VCC  - +3.3 V
+// GND  - Ground
+// !SCL - PA2 Sclk SPI clock from microcontroller to TFT or SDC
+// !SDA - PA5 MOSI SPI data from microcontroller to TFT or SDC
+// DC   - PA6 TFT data/command
+// RES  - PA7 TFT reset
+// CS   - PA3 TFT_CS, active low to enable TFT
+// *CS  - (NC) SDC_CS, active low to enable SDC
+// MISO - (NC) MISO SPI data from SDC to microcontroller
+// X– (NC) analog input X-axis from ADXL335 accelerometer
+// Y– (NC) analog input Y-axis from ADXL335 accelerometer
+// Z– (NC) analog input Z-axis from ADXL335 accelerometer
+// Backlight + - Light, backlight connected to +3.3 V
+
+#include <stdio.h>
+#include <stdint.h>
+#include "ST7735.h"
+#include "PLL.h"
+#include "tm4c123gh6pm.h"
+#include "adc.h"
+
+#define TOP       1   
+#define BOTTOM    0
+#define SAMPLENUM 8    // number of samples in a set collected from ADC_Collect
+#define CHANNEL0  0    // sample ADC channel 0 (part4 command: adc 0 )
+#define CHANNEL3  3    // sample ADC channel 3 (part4 command: adc 3 )
+#define SAMPLE_F  1000 // sampling frequency in Hz
+
+void DelayWait10ms(uint32_t n);
+
+int main (void)
+{
+	PLL_Init(Bus80MHz);              // set system clock to 80 MHz
+	ST7735_InitR(INITR_REDTAB);      // initialize a ST7735R chip, red tab
+	ADC_Open();                      // Initialize ADC sampling
+  uint32_t ADC_samples[SAMPLENUM]; // for storing sample set 
+	
+	//-----------display single shot ADC sample on top half------------//
+	
+	ST7735_Message(TOP, 0, "One-shot ADC value:", ST7735_WHITE, NULL);
+	ST7735_Message(TOP, 2, " ", ST7735_WHITE, ADC_In(0));
+	
+	//-----collect a set of samples and display on the bottom half-----//
+	
+	ADC_Collect(CHANNEL0, SAMPLE_F, ADC_samples, SAMPLENUM); // collect 8 samples from channel0 at 10 Hz, and store the results in ADC_samples
+	uint8_t lineIndex;                        // sample index in ADC_samples[] correspondes to the line number in which it will be displayed
+	for(lineIndex = 0; lineIndex < 8; lineIndex++)
+	{
+		ST7735_Message(BOTTOM, lineIndex, " ", ST7735_WHITE, ADC_samples[lineIndex]);
+	}
+}
+
+
+#define PF2   (*((volatile uint32_t *)0x40025010))
+
+// Make PF2 an output, enable digital I/O, ensure alt. functions off
+void SSR_Init(void){
+  SYSCTL_RCGCGPIO_R |= 0x20;        // 1) activate clock for Port F
+  while((SYSCTL_PRGPIO_R&0x20)==0){}; // allow time for clock to start
+                                    // 2) no need to unlock PF2
+  GPIO_PORTF_PCTL_R &= ~0x00000F00; // 3) regular GPIO
+  GPIO_PORTF_AMSEL_R &= ~0x04;      // 4) disable analog function on PF2
+  GPIO_PORTF_DIR_R |= 0x04;         // 5) set direction to output
+  GPIO_PORTF_AFSEL_R &= ~0x04;      // 6) regular port function
+  GPIO_PORTF_DEN_R |= 0x04;         // 7) enable digital port
+}
+
+
+void Delay1ms(uint32_t n);
+int main4(void){
+  SSR_Init();
+  while(1){
+    Delay1ms(10);
+    PF2 ^= 0x04;
+  }
+}
+
+// Subroutine to wait 10 msec
+// Inputs: None
+// Outputs: None
+// Notes: ...
+void DelayWait10ms(uint32_t n){uint32_t volatile time;
+  while(n){
+    time = 727240*2/91;  // 10msec
+    while(time){
+      time--;
+    }
+    n--;
+  }
+}
