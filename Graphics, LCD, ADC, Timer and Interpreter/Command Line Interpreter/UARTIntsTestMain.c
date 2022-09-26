30ea44168620d74408c52949db65293222a771de
Yilei Li <97814993+Riverflame-dev@users.noreply.github.com>
2022-09-26 16:07:16 -0400

    Add files via upload

---
+++ Graphics, LCD, ADC, Timer and Interpreter/Command Line Interpreter/UARTIntsTestMain.c
@@ -0,0 +1,80 @@
+// UARTIntsTestMain.c
+// Runs on LM4F120/TM4C123
+// Tests the UART0 to implement bidirectional data transfer to and from a
+// computer running HyperTerminal.  This time, interrupts and FIFOs
+// are used.
+// Daniel Valvano
+// September 12, 2013
+
+/* This example accompanies the book
+   "Embedded Systems: Real Time Interfacing to Arm Cortex M Microcontrollers",
+   ISBN: 978-1463590154, Jonathan Valvano, copyright (c) 2015
+   Program 5.11 Section 5.6, Program 3.10
+
+ Copyright 2015 by Jonathan W. Valvano, valvano@mail.utexas.edu
+    You may use, edit, run or distribute this file
+    as long as the above copyright notice remains
+ THIS SOFTWARE IS PROVIDED "AS IS".  NO WARRANTIES, WHETHER EXPRESS, IMPLIED
+ OR STATUTORY, INCLUDING, BUT NOT LIMITED TO, IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE APPLY TO THIS SOFTWARE.
+ VALVANO SHALL NOT, IN ANY CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL,
+ OR CONSEQUENTIAL DAMAGES, FOR ANY REASON WHATSOEVER.
+ For more information about my classes, my research, and my books, see
+ http://users.ece.utexas.edu/~valvano/
+ */
+
+// U0Rx (VCP receive) connected to PA0
+// U0Tx (VCP transmit) connected to PA1
+
+#include <stdint.h>
+#include <stdio.h> 
+#include <string.h> 
+#include "PLL.h"
+#include "UART.h"
+#include "adc.h"
+#include "ST7735.h"
+
+//---------------------OutCRLF---------------------
+// Output a CR,LF to UART to go to a new line
+// Input: none
+// Output: none
+void OutCRLF(void){
+  UART_OutChar(CR);
+  UART_OutChar(LF);
+}
+
+
+//debug code
+int main(void){
+  char i;
+  char string[20];             // global to assist in debugging
+  uint32_t n;
+
+  PLL_Init(Bus80MHz);          // set system clock to 80 MHz	
+	ST7735_InitR(INITR_REDTAB);  // initialize a ST7735R chip, red tab
+	
+  UART_Init();                 // initialize UART
+	
+	
+	// we probably don't need this following stuff
+  OutCRLF();
+  for(i='A'; i<='Z'; i=i+1){   // print the uppercase alphabet
+    UART_OutChar(i);
+  }
+  OutCRLF();
+  UART_OutChar(' ');
+  for(i='a'; i<='z'; i=i+1){   // print the lowercase alphabet
+    UART_OutChar(i);
+  }
+  OutCRLF();
+  UART_OutChar('-');
+  UART_OutChar('-');
+  UART_OutChar('>');
+	
+	
+  while(1){
+    ADC_Open();                  // Initialize ADC sampling
+		Command_Run(); // this will constantly be called to aquire each char entered into command line
+		
+  }
+}
